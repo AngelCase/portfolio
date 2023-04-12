30e589904471f1c03b5487849e32cc8c9be61f92
AngelCase <inoino884@gmail.com>
2023-04-12 16:59:36 +0900

    add generate config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	nitro: {
+		output: {
+			publicDir: '../publish'
+		}
+	},	
+	app: {
+		baseURL: '/portfollio/'
+	}
 })
