236d4d56a9d2e46663556fde3dd265fea278f861
AngelCase <inoino884@gmail.com>
2023-04-14 09:31:39 +0900

    add eslint and prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	nitro: {
-		output: {
-			publicDir: '../docs'
-		}
-	},	
-	app: {
-		baseURL: '/portfollio/'
-	}
+  nitro: {
+    output: {
+      publicDir: '../docs'
+    }
+  },
+  app: {
+    baseURL: '/portfollio/'
+  }
 })
