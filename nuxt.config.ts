a71f1ec93c3a6c7b33a11871ae24750ea9f2c852
AngelCase <inoino884@gmail.com>
2023-04-22 15:17:57 +0900

    set language

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  head: {
+    htmlAttrs: {
+      lang: 'ja'
+    }
+  },
   nitro: {
     output: {
       publicDir: '../docs'
