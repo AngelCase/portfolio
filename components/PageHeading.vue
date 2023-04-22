f671a8b789d1267b56ea64a78131db0b5ba2563a
AngelCase <inoino884@gmail.com>
2023-04-22 17:59:53 +0900

    cut out heading as a component

---
+++ components/PageHeading.vue
@@ -0,0 +1,13 @@
+<script setup>
+  const props = defineProps({
+    text: String
+  })
+  const { scramble, current } = useTypeScramble(props.text)
+  scramble()
+</script>
+
+<template>
+  <div>
+    <h1>{{ current }}</h1>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,3 @@
-<script setup>
-  const text = 'This is my portfolio. The content does not yet exist.'
-  const { scramble, current } = useHackScramble(text)
-  scramble()
-</script>
-
 <template>
-  <div>
-    <p>{{ current }}</p>
-  </div>
+  <PageHeading text="WELCOME TO MY PORTFOLIO"></PageHeading>
 </template>
