d249153651602fe41b04ce35d22332f15c3bbcf7
AngelCase <inoino884@gmail.com>
2023-04-23 10:22:18 +0900

    add skills page

---
+++ components/SkillListItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+  const props = defineProps({
+    main: String,
+    subItems: Array
+  })
+
+  console.log(props)
+  const { scramble, current: mainText } = useHackScramble(props.main)
+  scramble()
+</script>
+
+<template>
+  <div>
+    <li :class="$style.mainItem">
+      {{ mainText }}
+    </li>
+    <ul :class="$style.subList" v-for="subItem in props.subItems">
+      <SkillSubListItem
+        :class="$style.subList__item"
+        :text="subItem"
+      ></SkillSubListItem>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" module>
+  .mainItem {
+    border-left: solid 2px #ccc;
+    padding-left: 8px;
+  }
+
+  .subList {
+    border-left: solid 2px #eee;
+    margin-top: 8px;
+    margin-bottom: 8px;
+    margin-left: 40px;
+    padding-left: 0px;
+    padding-right: 64px;
+    list-style: none;
+
+    &__item {
+      padding-left: 8px;
+    }
+  }
+</style>
---
+++ components/SkillSubListItem.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const props = defineProps({
+    text: String
+  })
+
+  console.log(props)
+  const { scramble, current: text } = useHackScramble(props.text)
+  scramble()
+</script>
+
+<template>
+  <div>
+    <li>{{ text }}</li>
+  </div>
+</template>
