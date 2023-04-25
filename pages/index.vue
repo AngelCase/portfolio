95b98a01c41f02230fb40802eb744310cfbd3196
AngelCase <60319060+AngelCase@users.noreply.github.com>
2023-04-25 23:00:34 +0900

    Revert "test dynamic import"

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,3 @@
-<script setup lang="ts">
-  const imageName = 'img-1'
-
-  let image = {}
-  try {
-    image = await import(`~/assets/img/${imageName}.gif`)
-  } catch (e) {
-    console.log(e)
-  }
-</script>
-
 <template>
   <PageHeading text="WELCOME TO MY PORTFOLIO"></PageHeading>
-  <img :src="image.default" />
 </template>
